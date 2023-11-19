c5467c26679221c7d1a8b1202c98aede63517a24
DuDuch23 <alexduduch77@gmail.com>
2023-11-19 02:11:07 +0100

    modif slider

--- public/js/slider.js
+++ public/js/slider.js
@@ -2,23 +2,51 @@ document.addEventListener("DOMContentLoaded", function () {
     const slides = document.querySelectorAll('.slider-item');
     let currentSlide = 0;
 
+    //l'autoslide
     function showSlide(index) {
         slides[currentSlide].classList.remove('active');
         currentSlide = (index + slides.length) % slides.length;
         slides[currentSlide].classList.add('active');
+
+        fadeIn(slides[currentSlide]);
     }
 
+    function fadeIn(element)
+    {
+        element.style.opacity = 0;
+        element.style.transition = 'opacity 1s';
+
+        setTimeout(function ()
+        {
+            element.style.opacity = 1;
+        }, 100);
+
+        setTimeout(function ()
+        {
+            fadeOut(slides[currentSlide]);
+        }, 4000);
+    }
+
+    function fadeOut(element)
+    {
+        element.style.transition = 'opacity 1s';
+        element.style.opacity = 0;
+
+        setTimeout(function () {
+            element.style.transition = '';
+        }, 1000);
+    }
+
+    //plus tard ajout d'un bouton slide suivante
     function nextSlide() {
         showSlide(currentSlide + 1);
     }
 
+    //plus tard ajout d'un bouton précédente slide
     function prevSlide() {
         showSlide(currentSlide - 1);
     }
 
-    // Auto slide change every 5000 milliseconds (5 seconds)
+    //c'est un autoslide de 5s
     setInterval(nextSlide, 5000);
-
-    // Optional: Add event listeners for manual navigation
-    // You can customize this based on your UI (e.g., add buttons to navigate)
 });
